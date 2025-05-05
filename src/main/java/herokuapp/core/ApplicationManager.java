a99a91fe9a9ba1a5c8b29871e35ccd7bded8d04b
natal <nataly.sib@gmail.com>
2025-05-05 20:20:11 +0200

    HW31

---
+++ src/main/java/herokuapp/core/ApplicationManager.java
@@ -0,0 +1,24 @@
+package herokuapp.core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ApplicationManager {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    protected void init() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.get("https://the-internet.herokuapp.com/");
+    }
+
+    protected void stop() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/herokuapp/core/TestBase.java
@@ -0,0 +1,20 @@
+package herokuapp.core;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase {
+
+    protected final ApplicationManager app = new ApplicationManager();
+
+    @BeforeMethod
+    public void setUp() {
+        app.init();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        app.stop();
+    }
+
+}
